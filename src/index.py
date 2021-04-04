e06d1c9d938755cfcf6f6cb959347ab5c2ef6e2f
Manuel Lorente Rodríguez <66995720+Manuel0516@users.noreply.github.com>
2021-04-04 21:51:24 +0200

    Update index.py

--- src/index.py
+++ src/index.py
@@ -33,4 +33,4 @@ async def on_ready():
     await bot.change_presence(activity=discord.Streaming(name="Hacer Stonks", url="http://www.hypixel.com"))
     print('My Bot is Ready')
  
-bot.run('ODI3OTg4NjY4NTc2NDMyMTU4.YGjCiQ.OVJZ8EzDk_3mB1Jj_RPJ4AIO3Qc')
+bot.run('')
